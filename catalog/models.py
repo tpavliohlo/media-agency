96a8e3899402f561f02c7ac6bb55b82a751f637a
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-01-03 10:14:24 +0100

    add models.py with classes Topic, Redactor, Newspaper

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateTimeField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(Redactor)
