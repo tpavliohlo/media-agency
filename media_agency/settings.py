e4df3f418ba3418a95e4087466d2f137e5677f3e
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-01-03 14:07:41 +0100

    create home page

--- media_agency/settings.py
+++ media_agency/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog',
 ]
 
 MIDDLEWARE = [
@@ -54,8 +55,7 @@ ROOT_URLCONF = 'media_agency.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+AUTH_USER_MODEL = "catalog.Redactor"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
