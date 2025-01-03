46ae60383d415ef18def25cd9d9dc132d87cfc63
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-01-03 12:49:05 +0100

    add index function to views

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from catalog.models import Redactor, Topic, Newspaper
+
+
 # Create your views here.
+def index(request):
+    """View function for home page."""
+
+    num_redactors = Redactor.objects.all().count()
+    num_topics = Topic.objects.all().count()
+    num_newspapers = Newspaper.objects.all().count()
+
+    context = {
+        'num_redactors': num_redactors,
+        'num_topics': num_topics,
+        'num_newspapers': num_newspapers,
+    }
+    return render(request, "index.html", context=context)
