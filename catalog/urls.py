18d1da3a2fa54307ccee6d390a08b9c1915894a8
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-01-04 15:22:38 +0100

    add urls Create/Update/DeleteView for Topic

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 
 from catalog.views import index, TopicListView, NewspaperListView, NewspaperDetailView, RedactorListView, \
-    RedactorDetailView
+    RedactorDetailView, TopicCreateView, TopicUpdateView, TopicDeleteView
 
 urlpatterns = [
     path('', index, name='index'),
     path('topics/', TopicListView.as_view(), name='topic-list'),
+    path('topics/create/', TopicCreateView.as_view(), name='topic-create'),
+    path('topics/<int:pk>/update/', TopicUpdateView.as_view(), name='topic-update'),
+    path('topics/<int:pk>/delete/', TopicDeleteView.as_view(), name='topic-delete'),
     path('newspapers/', NewspaperListView.as_view(), name='newspaper-list'),
     path('newspapers/<int:pk>/', NewspaperDetailView.as_view(), name='newspaper-detail'),
     path('redactors/', RedactorListView.as_view(), name='redactor-list'),
