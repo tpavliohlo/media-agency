128a95528e0ca37383f6a79ea4e7a43a8023301e
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-01-04 15:49:12 +0100

    create Create/Update/Delete classes for Newspaper model

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path, include
 
-from catalog.views import index, TopicListView, NewspaperListView, NewspaperDetailView, RedactorListView, \
-    RedactorDetailView, TopicCreateView, TopicUpdateView, TopicDeleteView
+from catalog.views import (
+    index,
+    TopicListView,
+    NewspaperListView,
+    NewspaperDetailView,
+    RedactorListView, \
+    RedactorDetailView,
+    TopicCreateView,
+    TopicUpdateView,
+    TopicDeleteView,
+    NewspaperCreateView,
+    NewspaperUpdateView, \
+    NewspaperDeleteView
+)
 
 urlpatterns = [
     path('', index, name='index'),
@@ -11,6 +23,9 @@ urlpatterns = [
     path('topics/<int:pk>/delete/', TopicDeleteView.as_view(), name='topic-delete'),
     path('newspapers/', NewspaperListView.as_view(), name='newspaper-list'),
     path('newspapers/<int:pk>/', NewspaperDetailView.as_view(), name='newspaper-detail'),
+    path("newspapers/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
+    path("newspapers/<int:pk>/update/", NewspaperUpdateView.as_view(), name="newspaper-update"),
+    path("newspapers/<int:pk>/delete/", NewspaperDeleteView.as_view(), name="newspaper-delete"),
     path('redactors/', RedactorListView.as_view(), name='redactor-list'),
     path('redactors/<int:pk>/', RedactorDetailView.as_view(), name='redactor-detail'),
 ]
